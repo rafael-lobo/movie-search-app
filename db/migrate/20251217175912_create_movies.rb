ec50833b5a3255cbdc2e0a25ecf35212bfef1018
Rafael Lobo <rafaellpalmeira@hotmail.com>
2025-12-17 18:12:57 +0000

    apply db migration

--- db/migrate/20251217175912_create_movies.rb
+++ db/migrate/20251217175912_create_movies.rb
@@ -1,14 +1,19 @@
 class CreateMovies < ActiveRecord::Migration[8.1]
   def change
     create_table :movies do |t|
-      t.integer :tmdb_id
-      t.string :title
+      t.integer :tmdb_id, null: false
+      t.string :title, null: false
       t.date :release_date
       t.text :overview
       t.string :poster_path
-      t.decimal :popularity
+      t.decimal :popularity, precision: 10, scale: 2
 
       t.timestamps
+
     end
+
+    # Manually adding contrains and indexes for better performance
+    add_index :movies, :tmdb_id, unique: true # prevent duplicates
+    add_index :movies, :release_date # will be queried often
   end
 end
